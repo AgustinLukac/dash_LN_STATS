cae671e012814541d3d0e18e32625744d9ce15e7
AgustinLukac <agustin_ruso2003@hotmail.com>
2025-01-03 21:09:42 -0300

    Tercer cambio

--- pages/page1.py
+++ pages/page1.py
@@ -217,18 +217,7 @@ layout = html.Div([
     indicators=True,
     style={"width": "15%", "margin": "0 auto"}),
 
-    #############################################
-    html.Div(
-    [
-        dbc.Alert("This is a primary alert", color="primary"),
-        dbc.Alert("This is a secondary alert", color="secondary"),
-        dbc.Alert("This is a success alert! Well done!", color="success"),
-        dbc.Alert("This is a warning alert... be careful...", color="warning"),
-        dbc.Alert("This is a danger alert. Scary!", color="danger"),
-        dbc.Alert("This is an info alert. Good to know!", color="info"),
-        dbc.Alert("This is a light alert", color="light"),
-        dbc.Alert("This is a dark alert", color="dark"),
-    ]),
+    
     
     
     #############################################
