ab625d5ca1407c15584eba70df2e4a9e927ecce0
AgustinLukac <agustin_ruso2003@hotmail.com>
2025-01-03 21:08:17 -0300

    Segunda Mod

--- pages/page2.py
+++ pages/page2.py
@@ -1,8 +1,21 @@
 from dash import html
+import dash_bootstrap_components as dbc
 
 layout = html.Div([
     html.H1('Página 2'),
     html.Div([
-        html.A('Volver a la Página Principal', href='/')
+        html.A('Volver a la Página Principal', href='/'),
+        #############################################
+    html.Div(
+    [
+        dbc.Alert("This is a primary alert", color="primary"),
+        dbc.Alert("This is a secondary alert", color="secondary"),
+        dbc.Alert("This is a success alert! Well done!", color="success"),
+        dbc.Alert("This is a warning alert... be careful...", color="warning"),
+        dbc.Alert("This is a danger alert. Scary!", color="danger"),
+        dbc.Alert("This is an info alert. Good to know!", color="info"),
+        dbc.Alert("This is a light alert", color="light"),
+        dbc.Alert("This is a dark alert", color="dark"),
+    ]),
     ])
 ])
